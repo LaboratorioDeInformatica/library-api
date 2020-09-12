5fe9490b55ec1785a73fdaaa89488f825f37215b
Hashimoto <labdeinf@gmail.com>
2020-09-11 22:01:44 -0300

    Mapper with ModelMapper

--- library-api/src/main/java/com/labinf/libraryapi/resource/BookController.java
+++ library-api/src/main/java/com/labinf/libraryapi/resource/BookController.java
@@ -3,6 +3,7 @@ package com.labinf.libraryapi.resource;
 import com.labinf.libraryapi.dto.BookDTO;
 import com.labinf.libraryapi.model.entity.Book;
 import com.labinf.libraryapi.service.BookService;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,16 +12,18 @@ import org.springframework.web.bind.annotation.*;
 public class BookController {
 
     private BookService service;
+    private ModelMapper modelMapper;
 
-    public BookController(BookService service) {
+    public BookController(BookService service, ModelMapper modelMapper) {
         this.service = service;
+        this.modelMapper = modelMapper;
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public BookDTO create(@RequestBody BookDTO dto) {
-        Book bookSave = Book.builder().author(dto.getAuthor()).title(dto.getTitle()).isbn(dto.getIsbn()).build();
+        Book bookSave =modelMapper.map(dto, Book.class);
         bookSave = service.save(bookSave);
-        return dto.builder().id(bookSave.getId()).author(bookSave.getAuthor()).title(bookSave.getTitle()).isbn(bookSave.getIsbn()).build();
+        return modelMapper.map(bookSave, BookDTO.class);
     }
 }
